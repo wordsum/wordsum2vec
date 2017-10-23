cd4d357f493c576bb8efb48d236d0076f3a1bcc2
Kalab Oster <kalaboster@gmail.com>
2017-10-23 10:17:28 -0700

    Write the beginning to reason how to create service for wordsum in python. Write this to begin. Publish an outline.

---
+++ wordsum2vec_app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import request
+
+application = Flask(__name__)
+
+@application.route("/api/v1.0/wordsum2vec", methods=['GET'])
+def get_permutations():
+
+    string_arg = request.args.get('file')
+
+
+
+
+if __name__ == "__main__":
+    application.run(host='0.0.0.0', debug=True, port=8080)
